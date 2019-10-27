7d8228f1609ff3fbacab7cdb4cdd01e6e0a929e3
Santana <44216456+D4rkSantana@users.noreply.github.com>
2019-10-27 17:53:40 -0300

    Update script01.py

--- 01-Variaveis/script01.py
+++ 01-Variaveis/script01.py
@@ -5,3 +5,8 @@ print(caixa_1)
 print("Digite o que vc quer guardar na caixa: ")
 caixa_1 = input()
 print("Voce guardou " + caixa_1 + " na caixa.")
+nome = input("qual seu nome?") 
+sobrenome = input("Qual seu sobrenome " + nome)
+nome_completo = nome + " " + sobrenome
+print("Olá " + nome_completo)
+
